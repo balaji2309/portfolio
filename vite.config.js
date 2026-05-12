cdcd62bf5ba58bb9757b11578c858d4e99937499
balaji2309 <balaji.k2022b@vitstudent.ac.in>
2026-05-12 22:59:04 +0530

    Fix: relative paths and base url for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures assets are served from the root
-  build: {
-    outDir: 'dist',
-  }
+  base: './', // Change this from '/' to './'
 })
